01791c8f5a90daf9a7723f478c1800ae059fd31c
NnQqDd <quangduy162003@gmail.com>
2026-01-26 20:28:36 +0000

    Add DDDM-VC

---
+++ DDDM-VC/main.py
@@ -0,0 +1,24 @@
+import subprocess
+import os
+
+
+source_path = "/home/duyn/ActableDuy/datasets/VCTK/VCTK-Corpus/VCTK-Corpus/wav48/p225"
+source_audios = [os.path.join(source_path, filename) for filename in os.listdir(source_path)]
+source_audios = source_audios[:200]
+source_audios.sort()
+print(source_audios)
+target_audio = "../reference_audio.wav"
+for idx, source_audio in enumerate(source_audios):    
+    cmd = [
+        "python3", "inference.py",
+        "--src_path", source_audio,
+        "--trg_path", target_audio,
+        "--ckpt_model", "./ckpt/model_base.pth",
+        "--ckpt_voc", "./vocoder/voc_ckpt.pth",
+        "--ckpt_f0_vqvae", "./f0_vqvae/f0_vqvae.pth",
+        "--output_dir", "../voice-conversion-audios",
+        "--index", str(idx),
+        "-t", "6",
+    ]
+
+    subprocess.run(cmd, check=True)
