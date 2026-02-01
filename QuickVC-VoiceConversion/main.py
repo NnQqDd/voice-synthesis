eb7bec993894e09992a8a6649820e14ee9154122
NnQqDd <quangduy162003@gmail.com>
2026-02-01 13:12:37 +0000

    Minor changes

--- QuickVC-VoiceConversion/main.py
+++ QuickVC-VoiceConversion/main.py
@@ -4,8 +4,8 @@ import os
 # Write audios to txt
 source_path = "/home/duyn/ActableDuy/datasets/VCTK/VCTK-Corpus/VCTK-Corpus/wav48/p225"
 source_audios = [os.path.join(source_path, filename) for filename in os.listdir(source_path)]
-source_audios = source_audios[:200]
 source_audios.sort()
+source_audios = source_audios[:200]
 print(source_audios)
 target_audio = "../reference_audio.wav"
 
